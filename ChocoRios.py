21ccd5342fda569dc36a0280e14707ad583e25df
lucas <lucascptel@gmail.com>
2024-04-07 13:40:25 -0300

    teste css

--- ChocoRios.py
+++ ChocoRios.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for, f
 import database
 
 app = Flask(__name__, static_url_path='/static')
-app.secret_key = 'your_secret_key_here'
+app.secret_key = 'your_secret_key'
 
 
 @app.route('/')
